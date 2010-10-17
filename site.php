30a9fd57d817634013a8040ffb3406577efd0f42
Jeffrey Way <jeffrey@jeffrey-way.com>
2010-10-17 12:10:41 -0400

    Added better error checking and formatting

--- site.php
+++ site.php
@@ -1,30 +1,41 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+// If "siteName" isn't in the querystring, set the default site name to 'nettuts'
 $siteName = empty($_GET['siteName']) ? 'nettuts' : $_GET['siteName'];
 
 $cache = dirname(__FILE__) . "/cache/$siteName";
+// Re-cache every three hours
 if(filemtime($cache) < (time() - 10800))
 {
    // Get from server
    if ( !file_exists(dirname(__FILE__) . '/cache') ) {
       mkdir(dirname(__FILE__) . '/cache', 0777);
    }
+   // YQL query (SELECT * from feed ... ) // Split for readability
+   $path = "http://query.yahooapis.com/v1/public/yql?q=";
+   $path .= urlencode("SELECT * FROM feed WHERE url='http://feeds.feedburner.com/$siteName'");
+   $path .= "&format=json";
 
-   $path = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20feed%20where%20url%3D'http%3A%2F%2Ffeeds.feedburner.com%2F$siteName'&format=json";
-
+   // Call YQL, and if the query didn't fail, cache the returned data
    $feed = file_get_contents($path, true);
 
-   $cachefile = fopen($cache, 'wb');
-   fwrite($cachefile, $feed);
-   fclose($cachefile);
+   // If something was returned, cache
+   if ( $feed->query->count ) {
+      $cachefile = fopen($cache, 'wb');
+      fwrite($cachefile, $feed);
+      fclose($cachefile);
+   }
 }
 else
 {
-   // Use cached file
+   // We already have local cache. Use that instead.
    $feed = file_get_contents($cache);
 }
 
+// Decode that shizzle
 $feed = json_decode($feed);
 
+// Include the view
 include('views/site.tmpl.php');
 
