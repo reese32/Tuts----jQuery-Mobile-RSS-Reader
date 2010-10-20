f8d21141293cfd026b306a5d3c0d1ae17fa10dce
Jeffrey Way <jeffrey@jeffrey-way.com>
2010-10-19 22:03:57 -0400

    Various bug fixes

--- views/article.tmpl.php
+++ views/article.tmpl.php
@@ -3,7 +3,7 @@
 
 <div data-role="page">
 
-   <header data-role="header">
+   <header data-role="header" class="<?php echo $siteName; ?>">
       <h1> <?php echo ucWords($siteName).'+'; ?> </h1>
    </header><!-- /header -->
 
@@ -12,7 +12,7 @@
         <div> <?php echo $feed->description; ?> </div>
    </div><!-- /content -->
 
-   <footer data-role="footer" data-position="fixed">
+   <footer data-role="footer" data-position="fixed" class="<?php echo $siteName; ?>">
       <h4> <a href="<?php echo $feed->guid->content;?>" data-icon="forward"> Read on <?php echo ucWords($siteName); ?>+</a></h4>
    </footer>
 </div><!-- /page -->
